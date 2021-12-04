e2db57d6a5bc71dd4111d48fad62521020f5a7c5
Arthur Ryoo <jene0523@gmail.com>
2021-12-03 21:10:42 -0800

    Add route and dependencies

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const pool = require('./db');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const todoRoute = require('./routes/todos');
+
+app.use(express.json());
+app.use(morgan('common'));
+app.use(helmet());
 
 dotenv.config();
+
+app.use('/api/todos', todoRoute);
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
